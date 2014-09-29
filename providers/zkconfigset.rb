70fb5f91f136936f7062c2d79fbbf9b36d7b395d
vkhatri <vir.khatri@gmail.com>
2014-09-29 13:01:29 +0000

    fix for #7

--- providers/zkconfigset.rb
+++ providers/zkconfigset.rb
@@ -47,14 +47,22 @@ action :create do
       files_mode  0644
       files_owner new_resource.user
       files_group new_resource.group
-      notifies    :run, "execute[zk_config_set_upconfig_#{new_resource.name}]", :immediately if node.solrcloud.notify_zkconfigsets_upload
+      notifies    :run, "execute[zk_config_set_upconfig_#{new_resource.name}_update_upload]", :immediately if node.solrcloud.notify_zkconfigsets_upload
       only_if     { node.solrcloud.manage_zkconfigsets_source }
     end
 
-    execute "zk_config_set_upconfig_#{new_resource.name}" do
+    # Upload on any config update
+    execute "zk_config_set_upconfig_#{new_resource.name}_update_upload" do
       command   "#{new_resource.solr_zkcli} -zkhost #{new_resource.zkhost} -cmd upconfig -confdir #{::File.join(new_resource.zkconfigsets_home, new_resource.name, 'conf')} -confname #{new_resource.name} 2>&1"
       action      :nothing
       only_if     { node.solrcloud.manage_zkconfigsets }
+    end
+
+    # Update if config is not present in zk, like attribute node.solrcloud.manage_zkconfigsets was not during the first chef run
+    execute "zk_config_set_upconfig_#{new_resource.name}_missing_upload" do
+      command   "#{new_resource.solr_zkcli} -zkhost #{new_resource.zkhost} -cmd upconfig -confdir #{::File.join(new_resource.zkconfigsets_home, new_resource.name, 'conf')} -confname #{new_resource.name} 2>&1"
+      action      :run
+      only_if     { node.solrcloud.manage_zkconfigsets and not SolrCloud::Zk.new(new_resource.zkhost).configset?(new_resource.name) }
     end
   end
 end
