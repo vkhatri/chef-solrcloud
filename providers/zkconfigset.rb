fadcd46a8d97c94e04edcc45af87f77f4fc90a79
Virender Khatri <vir.khatri@gmail.com>
2015-04-25 15:10:34 +0530

    issue #32  - providers zkconfigset must refer to attribute :configset_name instead of :name

--- providers/zkconfigset.rb
+++ providers/zkconfigset.rb
@@ -24,13 +24,13 @@ end
 use_inline_resources
 
 action :delete do
-  converge_by("delete config set #{new_resource.name} if exists \n") do
-    execute "zk_config_set_rmr_#{new_resource.name}" do
-      command "echo 'rmr /configs/#{new_resource.name}' | #{new_resource.zkcli} -server #{new_resource.zkhost} 2>&1"
-      only_if { new_resource.manage_zkconfigsets && SolrCloud::Zk.new(new_resource.zkhost).configset?(new_resource.name) }
+  converge_by("delete config set #{new_resource.configset_name} if exists \n") do
+    execute "zk_config_set_rmr_#{new_resource.configset_name}" do
+      command "echo 'rmr /configs/#{new_resource.configset_name}' | #{new_resource.zkcli} -server #{new_resource.zkhost} 2>&1"
+      only_if { new_resource.manage_zkconfigsets && SolrCloud::Zk.new(new_resource.zkhost).configset?(new_resource.configset_name) }
     end
 
-    directory ::File.join(new_resource.zkconfigsets_home, new_resource.name) do
+    directory ::File.join(new_resource.zkconfigsets_home, new_resource.configset_name) do
       recursive true
       action :delete
       only_if     { node['solrcloud']['manage_zkconfigsets_source'] }
@@ -39,30 +39,30 @@ action :delete do
 end
 
 action :create do
-  converge_by("create config set #{new_resource.name} if missing \n") do
-    remote_directory ::File.join(new_resource.zkconfigsets_home, new_resource.name) do
+  converge_by("create config set #{new_resource.configset_name} if missing \n") do
+    remote_directory ::File.join(new_resource.zkconfigsets_home, new_resource.configset_name) do
       cookbook new_resource.zkconfigsets_cookbook
-      source new_resource.name
+      source new_resource.configset_name
       owner new_resource.user
       group new_resource.group
       mode 0644
       files_mode 0644
       files_owner new_resource.user
       files_group new_resource.group
-      notifies :run, "execute[zk_config_set_upconfig_#{new_resource.name}_update_upload]", :immediately if node['solrcloud']['notify_zkconfigsets_upload']
+      notifies :run, "execute[zk_config_set_upconfig_#{new_resource.configset_name}_update_upload]", :immediately if node['solrcloud']['notify_zkconfigsets_upload']
       only_if     { node['solrcloud']['manage_zkconfigsets_source'] }
     end
 
     # Upload on any config update
-    execute "zk_config_set_upconfig_#{new_resource.name}_update_upload" do
-      command "#{new_resource.solr_zkcli} -zkhost #{new_resource.zkhost} -cmd upconfig -confdir #{::File.join(new_resource.zkconfigsets_home, new_resource.name, 'conf')} -confname #{new_resource.configset_name} 2>&1"
+    execute "zk_config_set_upconfig_#{new_resource.configset_name}_update_upload" do
+      command "#{new_resource.solr_zkcli} -zkhost #{new_resource.zkhost} -cmd upconfig -confdir #{::File.join(new_resource.zkconfigsets_home, new_resource.configset_name, 'conf')} -confname #{new_resource.configset_name} 2>&1"
       action :nothing
       only_if     { node['solrcloud']['manage_zkconfigsets'] }
     end
 
     # Update if config is not present in zk, like attribute node['solrcloud']['manage_zkconfigsets'] was not during the first chef run
-    execute "zk_config_set_upconfig_#{new_resource.name}_missing_upload" do
-      command "#{new_resource.solr_zkcli} -zkhost #{new_resource.zkhost} -cmd upconfig -confdir #{::File.join(new_resource.zkconfigsets_home, new_resource.name, 'conf')} -confname #{new_resource.configset_name} 2>&1"
+    execute "zk_config_set_upconfig_#{new_resource.configset_name}_missing_upload" do
+      command "#{new_resource.solr_zkcli} -zkhost #{new_resource.zkhost} -cmd upconfig -confdir #{::File.join(new_resource.zkconfigsets_home, new_resource.configset_name, 'conf')} -confname #{new_resource.configset_name} 2>&1"
       action :run
       only_if { node['solrcloud']['manage_zkconfigsets'] && (new_resource.force_upload || !SolrCloud::Zk.new(new_resource.zkhost).configset?(new_resource.configset_name)) }
     end
