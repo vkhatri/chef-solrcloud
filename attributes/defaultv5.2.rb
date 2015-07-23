4c55ed1096114fc81f2eaff7795a88d208ac8103
Virender Khatri <vir.khatri@gmail.com>
2015-07-23 16:57:05 +0530

    issue #51, add new default attribute file for v5.2.1

---
+++ attributes/defaultv5.2.rb
@@ -0,0 +1,27 @@
+# solr v5.2.1+ specific attributes
+#
+
+default['solrcloud']['solr_config']['gc_log_opts'] = [] # %w(-verbose:gc -XX:+PrintHeapAtGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -XX:+PrintTenuringDistribution -XX:+PrintGCApplicationStoppedTime)
+
+default['solrcloud']['solr_config']['gc_tune'] = %w(-XX:NewRatio=3
+                                                    -XX:SurvivorRatio=4
+                                                    -XX:TargetSurvivorRatio=90
+                                                    -XX:MaxTenuringThreshold=8
+                                                    -XX:+UseConcMarkSweepGC
+                                                    -XX:+UseParNewGC
+                                                    -XX:ConcGCThreads=4
+                                                    -XX:ParallelGCThreads=4
+                                                    -XX:+CMSScavengeBeforeRemark
+                                                    -XX:PretenureSizeThreshold=64m
+                                                    -XX:+UseCMSInitiatingOccupancyOnly
+                                                    -XX:CMSInitiatingOccupancyFraction=50
+                                                    -XX:CMSMaxAbortablePrecleanTime=6000
+                                                    -XX:+CMSParallelRemarkEnabled
+                                                    -XX:+ParallelRefProcEnabled)
+
+# whether to use node fqdn or ip addressfor solr host
+default['solrcloud']['solr_config']['solr_host_type'] = 'fqdn' # options: fqdn, ip
+# fallback to node ip address if node fqdn is not set
+default['solrcloud']['solr_config']['solr_host'] = node[node['solrcloud']['solr_config']['solr_host_type']] || node['ip']
+
+default['solrcloud']['solr_config']['solr_timezone'] = 'UTC'
